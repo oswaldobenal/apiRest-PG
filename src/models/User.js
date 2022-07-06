de22b3ee6b8d463579c79124d5a6df12fbb303fe
Germanj18 <germnaguilera@gmail.com>
2022-07-06 13:02:13 -0300

    models/Rol/Pest/User

---
+++ src/models/User.js
@@ -0,0 +1,54 @@
+
+import { DataTypes } from "sequelize";
+import { sequelize } from "../database/database.js";
+import { Pets } from "./Pets.js";
+import { Rol } from "./Rol.js";
+
+
+export const User = sequelize.define('user', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  lastName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  countrie:{
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  city:{
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  email:{
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  password:{
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  }
+
+
+
+}, {
+  timestamps: false
+})
+User.belongsToMany(Rol,{through:"user_rol"})
+Rol.belongsToMany(User,{through:"user_rol"})
+User.hasMany(Pets);
+Pets.hasOne(User);
+
