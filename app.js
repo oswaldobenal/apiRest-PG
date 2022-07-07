1e18fb66d3b9be3a8ccdf2224c60fa28c4438c22
Germanj18 <germnaguilera@gmail.com>
2022-07-07 20:59:53 -0300

    userRoute

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 import express from "express";
+import morgan from "morgan";
 
 const app = express();
 // Routes import
 import countries from "./src/routes/country.routes.js"
+import pets from "./src/routes/pets.routes.js"
 import welcome from "./src/routes/routes.js"
+import user from "./src/routes/user.routes.js"
 //middleware
 app.use(express.json());
+app.use(morgan('dev'));
 //use routes
 app.use("/api/v1.0", welcome);
 app.use("/api/v1.0", countries);
+app.use("/api/v1.0", pets);
+app.use("/api/v1.0", user);
 
 export default app;
---
+++ src/routes/user.routes.js
@@ -0,0 +1,23 @@
+import { Router } from "express";
+//Import controllers
+import { createUser, getDetailUser, getUser, putUser } from "../controllers/useRuteController.js";
+import { body} from "express-validator"
+const router = Router();
+
+  router.post("/user", [
+    body('name').trim().notEmpty().withMessage('name is required'),
+    body('lastName').trim().notEmpty().withMessage('lastName is required'),
+    body('email',"Wrong email format").trim().isEmail().normalizeEmail(),
+    body('password').trim().notEmpty().withMessage('password is required'),
+  ], createUser);
+  router.get("/user",getUser);
+  router.get("/user/:id",getDetailUser);
+  router.put("/user/:id",[
+    body('name').trim().notEmpty().withMessage('name is required'),
+    body('lastName').trim().notEmpty().withMessage('lastName is required'),
+    body('password').trim().notEmpty().withMessage('password is required'),
+   
+  ],putUser);
+  
+ 
+export default router;
