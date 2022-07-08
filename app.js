fef1bb0d86bd2aee93e41ecdeacd52bda6caba16
Julian Benalcazar <oswaldobenal12@gmail.com>
2022-07-07 22:20:37 -0500

    Resolving fix

--- app.js
+++ app.js
@@ -6,14 +6,12 @@ const app = express();
 
 import countries from "./src/routes/country.routes.js";
 import welcome from "./src/routes/routes.js";
-import countries from "./src/routes/country.routes.js"
-import pets from "./src/routes/pets.routes.js"
-import welcome from "./src/routes/routes.js"
-import user from "./src/routes/user.routes.js"
+import pets from "./src/routes/pets.routes.js";
+import user from "./src/routes/user.routes.js";
 import cities from "./src/routes/cities.routes.js";
 //middleware
 app.use(express.json());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 //use routes
 app.use("/api/v1.0", welcome);
 app.use("/api/v1.0", countries);
