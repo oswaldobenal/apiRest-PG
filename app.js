144212780147a6d389b74095b4dd03c8fd2b5b49
Julian Benalcazar <oswaldobenal12@gmail.com>
2022-07-07 21:01:05 -0500

    Route getCountries and Route getCities using the id of Country

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ import express from "express";
 
 const app = express();
 // Routes import
-import countries from "./src/routes/country.routes.js"
-import welcome from "./src/routes/routes.js"
+import countries from "./src/routes/country.routes.js";
+import welcome from "./src/routes/routes.js";
+import cities from "./src/routes/cities.routes.js";
 //middleware
 app.use(express.json());
 //use routes
 app.use("/api/v1.0", welcome);
 app.use("/api/v1.0", countries);
+app.use("/api/v1.0", cities);
 
 export default app;
