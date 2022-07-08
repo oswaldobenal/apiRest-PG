8bb1bed5fd7b0b82bfcbbf945400a2ff74774461
Germanj18 <germnaguilera@gmail.com>
2022-07-08 13:07:20 -0300

    relaciones_con_modelos_detalles_en_rutas

--- src/controllers/useRuteController.js
+++ src/controllers/useRuteController.js
@@ -1,22 +1,28 @@
 import {validationResult} from "express-validator";
+import { City } from "../models/City.js";
+import { Country } from "../models/Country.js";
+import { Pets } from "../models/Pets.js";
 
 import { User } from "../models/User.js";
 
 /// POST USER
 export const createUser =async (req, res)  => {
     const errors = validationResult(req);
-    const{ name,lastName, password,email,isFundation,active,donaciones=0}=req.body;
+    const{ name,lastName, password,email,isFundation,active,donaciones=0,countryId,cityId}=req.body;
+    const country= await Country.findByPk(countryId)
+    const city= await City.findByPk(cityId)
     try {
     if (!errors.isEmpty()) { return res.status(400).json({ errors: errors.array()});}
     let verifyEmail= await User.findOne({
         where:{email:email}
     })
     if(verifyEmail){
-        res.status(400).json("the email entered already exists ");
+        res.status(400).json({ message:"the email entered already exists "});
     }else{
-     await User.create({ name, lastName,password,email,isFundation,active,donaciones })
-
-     res.json("User Cread")
+    let usersCountry= await User.create({ name, lastName,password,email,isFundation,active,donaciones })
+        usersCountry.setCountry(country)
+        usersCountry.setCity(city)
+     res.json({message:"User Cread"})
     }
      } catch (error) {
      res.send(error)
@@ -38,7 +44,9 @@ export const createUser =async (req, res)  => {
  let user;
      try {
          if(id.length>=1){
-        user= await User.findByPk(id)
+        user= await User.findByPk(id, { include: Pets })
+       let pets= await Pets.findAll({where:{userId:id}})
+       let city=await City.findByPk(user.cityId)
         if(user){
             if(user.isFundation===true){
             user= {
@@ -46,18 +54,24 @@ export const createUser =async (req, res)  => {
              lastName:user.lastName,
              email:user.email,
              isFundation:user.isFundation,
-             donaciones:user.donaciones
+             donaciones:user.donaciones,
+             country:user.countryId,
+             city:city.name,
+             pets:pets.map(e=>e)
             }
         }else{
             user= {
                 name:user.name,
                 lastName:user.lastName,
                 email:user.email,
+                country:user.countryId,
+                city:city.name,
+                pets:pets.map(e=>e)
                }
         }
          res.send(user);
          }else{
-            return res.status(400).json("user id does not exist")
+            return res.status(400).json({ message: "user id does not exist!" })
          }
         } 
      } catch (error) { 
@@ -76,9 +90,9 @@ export const putUser= async(req,res)=>{
                 id,
             }
           })
-       res.status(200).json("Updated!")
+          return res.status(201).json({ message: 'Updated!' });
         } catch (error) {
-         console.log(error)
+         
           return res.status(400).json({ message: error.message })
        }
     }
