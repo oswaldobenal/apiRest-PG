802a0349bc08712cec13e6b850493505066bd033
Julian Benalcazar <oswaldobenal12@gmail.com>
2022-07-07 21:33:00 -0500

    Route Get Cities by Country Id

--- app.js
+++ app.js
@@ -4,19 +4,12 @@ const app = express();
 // Routes import
 import countries from "./src/routes/country.routes.js";
 import welcome from "./src/routes/routes.js";
-
-
-
-
 import cities from "./src/routes/cities.routes.js";
 //middleware
 app.use(express.json());
 //use routes
 app.use("/api/v1.0", welcome);
 app.use("/api/v1.0", countries);
-
-
-
 app.use("/api/v1.0", cities);
 
 export default app;
