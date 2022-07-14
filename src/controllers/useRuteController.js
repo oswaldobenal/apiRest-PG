f778355ad9e43ef4bc49723234212ef17be1496d
Julian Benalcazar <oswaldobenal12@gmail.com>
2022-07-14 18:25:35 -0500

    Fix Route Update User

--- src/controllers/useRuteController.js
+++ src/controllers/useRuteController.js
@@ -8,8 +8,10 @@ import { Solicitudes } from "../models/Solicitudes.js";
 
 /// POST USER
 export const createUser = async (req, res) => {
-  const documentfile= req.files.map(d=>d.path);
-  const idfiles = req.files.map(d=>d.filename.slice(d.filename.lastIndexOf('/')+1))
+  const documentfile = req.files.map((d) => d.path);
+  const idfiles = req.files.map((d) =>
+    d.filename.slice(d.filename.lastIndexOf("/") + 1)
+  );
   const {
     name,
     lastName,
@@ -33,70 +35,73 @@ export const createUser = async (req, res) => {
     if (user === null) {
       const country = await Country.findByPk(countryId);
       const city = await City.findByPk(cityId);
-      //Hash of password.
-      const passwordHash = await encrypt(password);
-      if(role==="fundation"){
-        const usersCountry = await User.create({
-        name,
-        lastName,
-        password: passwordHash,
-        email,
-        role,
-        active:false,
-        document:documentfile[0]
-      });
-      //password set in undefined for security
-      usersCountry.set("password", undefined, { strict: false });
-      usersCountry.setCountry(country);
-      usersCountry.setCity(city);
-      Solicitudes.create({
-        userId:usersCountry.id
-      });
-      return res.json({
-        message:
-          "User Created Successfully!, If you solicited a verification of fundation the state is pending",
-      })
+      if (country && city) {
+        //Hash of password.
+        const passwordHash = await encrypt(password);
+        switch (role) {
+          case "fundation":
+            const userFundation = await User.create({
+              name,
+              lastName,
+              password: passwordHash,
+              email,
+              role,
+              active: false,
+              document: documentfile[0],
+            });
+            //password set in undefined for security
+            userFundation.set("password", undefined, { strict: false });
+            userFundation.setCountry(country);
+            userFundation.setCity(city);
+            Solicitudes.create({
+              userId: userFundation.id,
+            });
+            res.json({
+              message:
+                "User Created Successfully!, If you solicited a verification of fundation the state is pending",
+            });
+            break;
+          case "admin":
+            const userAdmin = await User.create({
+              name,
+              lastName,
+              password: passwordHash,
+              email,
+              role,
+            });
+            //password set in undefined for security
+            userAdmin.set("password", undefined, { strict: false });
+            userAdmin.setCountry(country);
+            userAdmin.setCity(city);
+            res.json({
+              message: "Admin Created Successfully!",
+            });
+            break;
+          default:
+            const user = await User.create({
+              name,
+              lastName,
+              password: passwordHash,
+              email,
+              active,
+            });
+            //password set in undefined for security
+            user.set("password", undefined, { strict: false });
+            user.setCountry(country);
+            user.setCity(city);
+            return res.json({
+              message: "User Created Successfully!",
+            });
+        }
       }
-      if(role==="admin"){
-        const usersCountry = await User.create({
-          name,
-          lastName,
-          password: passwordHash,
-          email,
-          role,
-        });
-        //password set in undefined for security
-        usersCountry.set("password", undefined, { strict: false });
-        usersCountry.setCountry(country);
-        usersCountry.setCity(city);
-        return res.json({
-          message:
-            "User Created Successfully!",
-        });
-      }if(role==="user"){
-         const usersCountry = await User.create({
-          name,
-          lastName,
-          password: passwordHash,
-          email,
-          role,
-          active,
-        });
-        //password set in undefined for security
-        usersCountry.set("password", undefined, { strict: false });
-        usersCountry.setCountry(country);
-        usersCountry.setCity(city);
-        return res.json({
-          message:
-            "User Created Successfully!",
-        });
-      }
+      return res.status(404).json({ error: "City and Country is required " });
+    } else {
+      deleteFile(idfiles);
+      return res.status(400).send({ Error: "email already exist!!" });
     }
-    deleteFile(idfiles);
-    return res.status(400).send({ Error: "email already exist!!" });
     // const data = {
-    //   token: await tokenSing(usersCountry),
-    //   user: usersCountry,
+    //   token: await tokenSing(userFundationCountry),
+    //   user: userFundationCountry,
     // };
     // return res.send(data);
   } catch (error) {
@@ -108,20 +113,20 @@ export const createUser = async (req, res) => {
 /// GET USER
 export const getUser = async (req, res) => {
   try {
-    let users = await User.findAll({
+    let userFundation = await User.findAll({
       attributes: {
         exclude: ["password"],
       },
-      include:{
+      include: {
         model: Solicitudes,
-        attributes:{
-          exclude:['updatedAt', 'createdAt']
-        }
-      }
+        attributes: {
+          exclude: ["updatedAt", "createdAt"],
+        },
+      },
     });
-    res.send(users);
+    return res.send(userFundation);
   } catch (error) {
-    res.json(error);
+    return res.json({ message: error.message });
   }
 };
 
@@ -130,11 +135,15 @@ export const getDetailUser = async (req, res) => {
   const { id } = req.params;
   try {
     if (id) {
-      const user = await User.findByPk(id, { include: Pets },{include: Solicitudes});
+      const user = await User.findByPk(
+        id,
+        { include: Pets },
+        { include: Solicitudes }
+      );
       if (user) {
         const pets = await Pets.findAll({ where: { userId: id } });
         const city = await City.findByPk(user.cityId);
-        const soli= await Solicitudes.findAll({where:{userId:id}});
+        const soli = await Solicitudes.findAll({ where: { userId: id } });
         const dataUser = {
           name: user.name,
           lastName: user.lastName,
@@ -148,7 +157,7 @@ export const getDetailUser = async (req, res) => {
           active: user.active,
           document: user.document,
           pets: pets.map((e) => e),
-          solcitudes:soli.map((e)=>e)
+          solcitudes: soli.map((e) => e),
         };
 
         return res.send(dataUser);
@@ -161,15 +170,37 @@ export const getDetailUser = async (req, res) => {
   }
 };
 
+const userDetail = async (id) => {
+  const user = await User.findByPk(id);
+  if (user) {
+    const city = await City.findByPk(user.cityId);
+    const country = await Country.findByPk(user.countryId);
+    const dataUser = {
+      name: user.name,
+      lastName: user.lastName,
+      email: user.email,
+      role: user.role,
+      country: country.name,
+      city: city.name,
+      address: user.address,
+      phone: user.phone,
+      active: user.active,
+    };
+    return dataUser;
+  } else {
+    return { error: "User Not Found" };
+  }
+};
 //UPDATE USER
 export const updateUser = async (req, res) => {
   const { id } = req.params;
-  const { password, newPassword } = req.body;
+  const { password, newPassword, cityId, countryId } = req.body;
   const us = await User.findOne({
     where: {
       id,
     },
   });
+
   try {
     if (password && newPassword) {
       const checkPassword = await compare(password, us.password);
@@ -187,13 +218,11 @@ export const updateUser = async (req, res) => {
           }
         );
 
-        return res.send({Ok: "Password Updated Successfully!!"});
+        return res.send({ Ok: "Password Updated Successfully!!" });
       } else {
-        return res
-          .status(401)
-          .json({
-            error: "Password Incorrect, Please insert your actual password",
-          });
+        return res.status(401).json({
+          error: "Password Incorrect, Please insert your actual password",
+        });
       }
     }
 
@@ -203,79 +232,61 @@ export const updateUser = async (req, res) => {
       },
     });
 
-    const userUpdated = await User.findOne({
-      where: {
-        id,
-      },
-      attributes: {
-        exclude: ["password"],
-      },
-    });
-    return res.send(userUpdated);
+    const user = await userDetail(id);
+    return res.send(user);
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
 };
 
-  //PUT USER from admin
+//PUT USER from admin
 export const adminUpdateUser = async (req, res) => {
-   const { id } = req.params;
-   const {
-           name,
-           lastName,
-           role,
-           address,
-           phone,
-           active,
-          } = req.body;
+  const { id } = req.params;
+  const { name, lastName, role, address, phone, active } = req.body;
   console.log(req.body);
-try {
-  await User.update(
-       {
-           name,
-           lastName,
-           role,
-           address,
-           phone,
-           active,
-       },
-       {
-        where:{
-               id,
-             },
+  try {
+    await User.update(
+      {
+        name,
+        lastName,
+        role,
+        address,
+        phone,
+        active,
+      },
+      {
+        where: {
+          id,
+        },
       }
-        );
-        return res.status(201).json({ message: "Updated!" });
-        } catch (error) {
-          return res.status(500).json({ message: error.message });
-        }
-      };
+    );
+    return res.status(201).json({ message: "Updated!" });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
 
 //PUT solicitud from admin
-  
-export const updatesolicitud = async (req, res) => {
-        const { id } = req.params;
-        const {
-               estado,
-               fechafinaliza
-            } = req.body;
-        try {
-          if (id) {
-            await Solicitudes.update(
-              {
-              estado,
-              fechafinaliza
-              },
-              { where: {
-                  id,
-                },
-              }
-            );
-            return res.status(201).json({ message: "State Updated!" });
-          }
-      
-        } catch (error) {
-          return res.status(500).json({ message: error.message });
-        }
-      };
 
+export const updatesolicitud = async (req, res) => {
+  const { id } = req.params;
+  const { estado, fechafinaliza } = req.body;
+  try {
+    if (id) {
+      await Solicitudes.update(
+        {
+          estado,
+          fechafinaliza,
+        },
+        {
+          where: {
+            id,
+          },
+        }
+      );
+      return res.status(201).json({ message: "State Updated!" });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
