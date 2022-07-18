52afa84c035b8ab427fb22e51983f409a4382c22
Julian Benalcazar <oswaldobenal12@gmail.com>
2022-07-17 19:22:24 -0500

    fix Countries Solved

--- src/controllers/countryController.js
+++ src/controllers/countryController.js
@@ -4,12 +4,12 @@ import { data } from "../database/countries.js";
 //This controller looks for the countries in the database and if they don't exist, it creates them
 export const getCountries = async (req, res) => {
   try {
-    const countries = data.countries.map((el) => {
+    const countries = data.countries.map((country) => {
       return {
-        id: el.iso3,
-        name: el.country,
-        currency: el.currency,
-        symbol: el.symbol,
+        id: country.iso3,
+        name: country.country,
+        currency: country.currency,
+        symbol: country.symbol,
       };
     });
     countries.forEach((country) => {
@@ -17,13 +17,13 @@ export const getCountries = async (req, res) => {
         where: {
           id: country.id,
           name: country.name,
-          currency: el.currency,
-          symbol: el.symbol,
+          currency: country.currency,
+          symbol: country.symbol,
         },
       });
     });
-    res.json(countries);
+    return res.json(countries);
   } catch (error) {
-    return res.status(500).json({ message: error.message });
+    return res.status(500).json({ messageE: error.message });
   }
 };
